816735e7f5ae14ee19b877b45d0b5694cd322de2
nerdscarf <tiph@nerdscarf.com>
2021-06-07 15:39:41 -0400

    Made Responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+
+        <div className="left">
+          <h1>Learn to code by watching others</h1>
+          <h4>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</h4>
+        </div>
+
+        <div className="right">
+          <div class="top-banner"><span>Try it free for 7 days </span>then $20/mo. there after</div>
+
+          <form>
+            <fieldset>
+                <label>
+                  <input type="text" name="fname" value="Jonathan" /><br />
+                  <input type="text" name="lname" value="Last Name" /><br />
+                  <input type="text" name="email" value="Email Address"/><br />
+                  <input type="text" name="password" value="Password"/><br />
+                  <input type="submit" value="Claim your free trial" />
+            <p>By clicking the button you are agree to our <span>Terms and Services</span></p>
+                </label>
+            </fieldset>
+          </form>
+
+
+        </div>
+
+
+      
+
+
+
+
+
+
+
+
+
+      </div>
     </div>
   );
 }
